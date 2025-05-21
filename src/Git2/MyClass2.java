8b2c230855df18b5760b1e12d8e913c2334f8f6b
lakki <motukatla.rdx@0248@gmail.com>
2025-05-21 11:31:50 +0530

    commit-1

---
+++ src/Git2/MyClass2.java
@@ -0,0 +1,14 @@
+package Git2;
+
+public class MyClass2 {
+
+	public static void main(String[] args) {
+System.out.println("C");
+
+	}
+	public void Ram() {
+		System.out.println("ramaRao");
+	}
+	
+
+}
